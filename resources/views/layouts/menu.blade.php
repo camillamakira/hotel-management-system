587ca7f516d4f9e5ddb3bb9c7df6f8627852d3ab
markmosobo <mmosobo@gmail.com>
2023-03-23 11:05:15 +0300

    changes

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -193,7 +193,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link">
+                 <a href="{{url('recreationorder-datatable')}}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Facility Orders</p>
                 </a>
